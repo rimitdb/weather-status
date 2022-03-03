89056ac91eef59bfd6930d67d008e6ed020204b2
Iqbal <iqbal.du.mis@gmail.com>
2022-03-03 15:05:39 +0600

    error handling done

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@ const api_key = `08ecb79e0a524a5fc6da150d99f4a34c`;
 
 
 const searchCity = () => {
-    const city = document.getElementById('search-box').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`;
+    const city = document.getElementById('search-box');
+    const searchText = city.value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&units=metric&appid=${api_key}`;
+    const errorMessage = document.getElementById('error-message');
+    if (searchText == '') {
+        errorMessage.style.display = 'block';
+        city.value = '';
+    } else {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayWeatherData(data));
+        errorMessage.style.display = 'none';
+    }
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayWeatherData(data));
 };
 
 const setInnerText = (id, text) => {
@@ -16,14 +24,22 @@ const setInnerText = (id, text) => {
 
 
 const displayWeatherData = temp => {
-    setInnerText('city-name', temp.name);
-    setInnerText('temp', temp.main.temp);
-    setInnerText('status', temp.weather[0].main);
-    setInnerText('description', temp.weather[0].description);
+    const errorMessage2 = document.getElementById('error-message2');
     console.log(temp);
-    //set weather icon
+    if (temp.cod == 404) {
+        errorMessage2.style.display = 'block';
+    } else {
+        setInnerText('city-name', temp.name);
+        setInnerText('temp', temp.main.temp);
+        setInnerText('status', temp.weather[0].main);
+        setInnerText('description', temp.weather[0].description);
 
-    const url = `https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
-    const imgIcon = document.getElementById('icon-img');
-    imgIcon.setAttribute('src', url);
+        //set weather icon
+        const url = `https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png`;
+        const imgIcon = document.getElementById('icon-img');
+        imgIcon.setAttribute('src', url);
+        errorMessage2.style.display = 'none';
+    }
+
+
 };
